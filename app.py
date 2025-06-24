dde3e790cbaa80e3af5b7e32be52a7d55864a88f
Godwin Samraj G <samrajgodwin7@gmail.com>
2025-06-24 20:15:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/13b-_2a7XGTveEEPUPzjs5WOCZZAObxCu
 """
 
-pip install pymupdf pandas
 
 # ================================
 # 📦 PACKAGE INSTALL & IMPORTS
